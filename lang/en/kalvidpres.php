26119886163a6e072403cb2baada79e33924aae1
Akinsaya Delamarre <adelamarre@remote-learner.net>
2012-12-12 15:13:15 -0500

    Adding 2.x code

---
+++ lang/en/kalvidpres.php
@@ -0,0 +1,28 @@
+<?php
+
+$string['modulenameplural'] = 'Kaltura Video Presentation';
+$string['modulename'] = 'Kaltura Video Presentation';
+$string['modulename_help'] = 'A video presentation creates a video that displays pages from a Power point presentation or a PDF at certain points during the video.'.
+                             '  First upload a video then upload a document.  <b>You must wait until the document has finished converting</b> before saving the activity.';
+$string['pluginadministration'] = 'Kaltura Video Presentation';
+$string['pluginname'] = 'Kaltura Video Presentation';
+$string['name'] = 'Name';
+$string['video_hdr'] = 'Video';
+$string['add_video'] = 'Add video';
+$string['upload_successful'] = 'Upload Successful';
+$string['video_converting'] = 'Your video is still converting.  Please check the status of your video at a later time.';
+$string['vide_preview'] = 'Preview';
+$string['invalidid'] = 'Invalid ID';
+$string['replace_video'] = 'Replace Video';
+$string['video_preview_header'] = 'Preview';
+$string['document_converting'] = 'Your document is still converting.  Please check the status of your document at a later time.';
+$string['add_document'] = 'Upload document';
+$string['check_status'] = 'Check status';
+$string['sync_document'] = 'Sync Keypoints';
+$string['update_video_pres_info'] = 'When updating the video or document for the video presentation, both the video and document must be uploaded again.';
+$string['view_vid_pres_btn'] = 'View video presentation';
+$string['vid_pres_incomplete'] = 'The video has been updated.  Please complete the steps and upload a document and wait for the document conversion to complete or cancel the update.';
+$string['pres_info'] = '<b>In order to create a video presentation resource, please upload a video and a document and wait for them to convert.'.
+                       'The conversion processes may take a few minutes depending on the sizes of the uploaded material. Once both video and '.
+                       'document are ready, click \'Preview\' in order to sync the document pages with the timeline of the video. Sync action is '.
+                       'also available once the resource is saved.</b>';
